d2416d890ea380eff00b71fe63bacce07547d067
Jukka Hell <jukka@hell.fi>
2014-08-12 14:16:25 +0300

    Loop webm videos

--- www/js/directives/MessageDirective.js
+++ www/js/directives/MessageDirective.js
@@ -12,7 +12,7 @@ angular.module('mafiachat.directives', []).directive('message', ['$rootScope', f
                scope.text = "<a class='imgThumb' target='_blank' href='"+scope.text+"'><img src='" + scope.text + "' height='100' /></a>";
                element.html(scope.text);
            } else if (/^http(s?):\/\/.+\.(webm)$/i.test(scope.text)) {
-               scope.text = '<video height="240" autoplay><source src="'+scope.text+'" type="video/webm"></video>';
+               scope.text = '<video height="240" loop autoplay><source src="'+scope.text+'" type="video/webm"></video>';
                element.html(scope.text);
            } else if (/(\b(https?):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/i.test(scope.text)) {
                var replacePattern = /(\b(https?):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/gim;
